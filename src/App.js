3c2fed8f0c1688793a57c12a590b6745774d0b20
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-07-27 09:16:42 -0300

    Finalizando projeto

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from '@mui/material';
 
-
-import {Navbar, VideoDetail, Feed, ChannelDetail, SearchFeed} from './components'
+import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
 
 const App = () => (
   <BrowserRouter>
-  <Box sx={{backgroundColor: '#000'}}>
-    <Navbar />
-    <Routes>
-      <Route path="/" exact element={<Feed />} />
-      <Route path="/video/:id" element={<VideoDetail />} />
-      <Route path="/channel/:id" element={<ChannelDetail />} />
-      <Route path="/search/:searchTerm" element={<SearchFeed />} />
-    </Routes>
-  </Box>
-    
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:searchTerm' element={<SearchFeed />} />
+      </Routes>
+    </Box>
   </BrowserRouter>
 );
 
